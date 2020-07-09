7392910c13b3624dbd2ac44509e3a7c5e57cf9a5
Eduardo Vizcaino <evigra@hotmail.com>
2020-07-09 10:04:45 -0500

    Update

--- taecel/models/model.py
+++ taecel/models/model.py
@@ -6,6 +6,10 @@ import random
 from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models, _
 
+from urllib.parse import urlencode
+import pycurl
+
+
 class taecel(models.Model):
     _name = "taecel"
     _description = 'TAECEL'
@@ -18,6 +22,30 @@ class taecel(models.Model):
     mensaje2                                    = fields.Char('mensaje2', size=150)
     status                                      = fields.Char('status', size=150)
     time                                        = fields.Datetime('Time')
-    def get_system_para(self):
-        para_value                              =self.env['ir.config_parameter'].get_param('solesgps_map_key','')
-        return para_value
+    def create(self,vals):
+        print('===========',vals)
+        taecel_key                              =self.env['ir.config_parameter'].get_param('taecel_key','')
+        taecel_nip                              =self.env['ir.config_parameter'].get_param('taecel_nip','')
+        
+        crl = pycurl.Curl()
+        crl.setopt(crl.URL, 'https://taecel.com/app/api/RequestTXN')
+        data = {
+            'key':          taecel_key,
+            'nip':          taecel_nip,
+            'producto':     taecel_nip,
+            'referencia':   taecel_nip,
+            'monto':        taecel_nip,
+        }
+        pf = urlencode(data)
+
+        """
+        # Sets request method to POST,
+        # Content-Type header to application/x-www-form-urlencoded
+        # and data to send in request body.
+        crl.setopt(crl.POSTFIELDS, pf)
+        crl.perform()
+        crl.close()                
+        """
+
+        return super(taecel, self).create(vals)
+        
